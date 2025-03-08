081b72724a365b2fd8d996430b0a5ae7485e0470
luizmrd <lipesilvamiranda@hotmail.com>
2025-03-08 12:05:58 -0300

    Att-java

---
+++ tipos-variasveis/Controle-de-fluxo/src/ExemploForArrays.java
@@ -0,0 +1,12 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+       // ExemploFor.java
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    
+    }
+    }
+
