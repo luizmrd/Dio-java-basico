3f1f48764191d3c216326678f6fcaa2d2c87afa0
luizmrd <lipesilvamiranda@hotmail.com>
2025-03-08 17:16:35 -0300

    Pronto

---
+++ tipos-variasveis/Desafios-java-basico/src/ProcessoSeletivo.java
@@ -0,0 +1,27 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo Seletivo");
+        
+        
+        double salarioCandidato = 2500.0;
+
+        if (salarioCandidato == 2000.0)
+            System.out.println("RH: Ligar para o candidato");
+
+        else if (salarioCandidato <= 2000.0)
+            System.out.println("RH: Ligar para o candidato e passar a contraproposta");
+
+        else 
+            System.out.println("RH: Candidado reprovado");
+            
+        }
+
+
+
+
+
+
+
+    }
+
+
